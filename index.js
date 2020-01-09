53da37b4e499f7ad44eddb4b658094efd5644678
rvargasm <arch.rvm@gmail.com>
2020-01-09 17:42:48 -0600

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import stylesheets
 import './style.css';
-import './populationchar.js'
+import './populationchar2.js'
 
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
--- populationchar2.js
+++ populationchar2.js
@@ -1,3 +1,6 @@
-d3.csv("https://drive.google.com/file/d/12oZV7PWG_UHn8hRe_8Ov4sNMdLDCQ6sW/view?usp=sharing").then(function(data) {
-  console.log(data[0]);
+d3.csv("https://drive.google.com/file/d/12oZV7PWG_UHn8hRe_8Ov4sNMdLDCQ6sW/view?usp=sharing", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i].Name);
+        console.log(data[i].Age);
+    }
 });
